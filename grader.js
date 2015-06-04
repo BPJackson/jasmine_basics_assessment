a10f8aa0b583f3cee3f5f42698ea5216301b8ca4
brianpjackson88@gmail.com <brianpjackson88@gmail.com>
2015-06-04 13:21:51 -0600

    finished assignment except multimode

---
+++ grader.js
@@ -0,0 +1,54 @@
+module.exports = {
+    letterGrader: function(a) {
+        if (a <= 100 && a >= 95)
+            return "A+";
+        if (a >= 90 && a < 95)
+            return "A-";
+        if (a >= 86 && a <= 89)
+            return "B+";
+        if (a >= 80 && a < 85)
+            return "B-";
+        if (a >= 76 && a <= 79)
+            return "C+";
+        if (a >= 70 && a < 75)
+            return "C-";
+        if (a >= 66 && a >= 69)
+            return "D+";
+        if (a >= 60 && a < 65)
+            return "D-";
+        if (a <= 59)
+            return "F";
+    },
+    averageScore: function(arr) {
+        sum = 0
+        for (var i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return (sum / arr.length);
+    },
+    medianScore:function(x) {
+        x.sort(function(a, b) {
+            return a - b;
+        });
+        var o = Math.floor(x.length / 2);
+        if (x.length % 2) {
+            return x[o];
+        } else {
+            return (x[o - 1] + x[o]) / 2.0;
+        }
+    },
+    modeScore: function mode(arr) {
+        var numMap = {};
+        var greatestFreq = 0;
+        var mode;
+        arr.forEach(function countMode(n) {
+            numMap[n] = (numMap[n] || 0) + 1;
+
+            if (greatestFreq <= numMap[n]) {
+                greatestFreq = numMap[n];
+                mode = n;
+            }
+        });
+        return +mode;
+    }
+}
---
+++ spec/spec.js
@@ -0,0 +1,36 @@
+var grader = require('./../grader.js');
+
+describe('Find the letter grade of a number grade', function(){
+  it('passing', function() {
+    expect(grader.letterGrader(98)).toEqual('A+');
+    expect(grader.letterGrader(90)).toEqual('A-');
+    expect(grader.letterGrader(89)).toEqual('B+');
+    expect(grader.letterGrader(79)).toEqual('C+');
+    expect(grader.letterGrader(69)).toEqual('D+');
+  });
+
+});
+describe('Find the average of an array', function(){
+  it('passing', function() {
+    expect(grader.averageScore([90, 95, 87, 60])).toEqual(83)
+    expect(grader.averageScore([1, 2, 3, 4])).toEqual(2.5)
+    expect(grader.averageScore([10, 20, 30,0 ])).toEqual(15)
+
+
+  });
+});
+describe('Find the median of an array', function(){
+  it('passing', function() {
+    expect(grader.medianScore([52,80,80,86,94])).toEqual(80)
+    expect(grader.medianScore([52,80,81,86])).toEqual(80.5)
+    expect(grader.medianScore([1,1,2,2,3])).toEqual(2)
+
+
+  });
+});
+describe('Find the mode of an array', function(){
+  it('passing', function() {
+    expect(grader.modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92)
+    expect(grader.modeScore([1,1,1,1,2,2,2,2,])).toEqual([1,2])
+
+  })});
